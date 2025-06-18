2d950f2705e881f84f660a1739f3c9d23ab8d2b9
Matteo Grandin <matteo.grandin@igi.cnr.it>
2025-06-18 15:29:41 +0200

    +

--- double_track_16_06_2025/runSIM.m
+++ double_track_16_06_2025/runSIM.m
@@ -12,6 +12,6 @@ u0                   = 0;
 v0                   = 0;
 yaw_rate0            = 0; %[rad/s] yaw rate at start
 
-
+    
 %% SIMULATION AND RESULTS
 out                = sim("DTM_sim.slx");
