0ddc862d12e60114dfd67a558f71cb1bf75a10ec
Marco-cortese <marcocortese1999@gmail.com>
2025-06-18 15:02:47 +0200

    DTM sim

---
+++ double_track_16_06_2025/initParam.m
@@ -0,0 +1,41 @@
+%% Workspace for Simulink Model
+% tommaso.belluco@studenti.unipd.it, 2055261
+clear; clc;close all;
+load Fufy.mat % file containing all data for car
+
+%% CHANGING SECTION
+% Manually update the library for the vehicle
+
+m        = 9.4+9.4; % [kg] veicolo con mini PC e tutto il resto
+b        = l/2; % stima del nuovo CoG
+a        = l-b;
+J_CoG    = 9.4*a^2+9.4*b^2; % [kg*m^2]
+Fz_Front = 9.4*g; % [N]
+Fz_Rear  = 9.4*g; % [N]
+Fz_Tot   = Fz_Rear + Fz_Front; % [N] 
+% Second test: height of CoG
+Fz_rear  = 9.8*g;
+H = 160/1000; % [m] 
+h = (Fz_rear*l/(Fz_Tot)-(l-b))*cot(asin(H/l))+(R_r+R_f)/2; % [m] new CoG height
+clear H Fz_rear Fz_Rear Fz_Front Fz_Tot
+
+%_____ Tires 
+R_fl                = R_f; %[m] front left wheel radius
+R_fr                = R_f; %[m] front right wheel radius
+R_rl                = R_r; %[m] rear left wheel radius
+R_rr                = R_r; %[m] rear right wheel radius
+
+%_____ starting position _ 
+x0                   = 0; %[m] distance CoG to y-axis (X0)
+y0                   = 0; %[m] distance CoG to x-axis (Y0)
+gamma                = pi/2; %[rad] starting angle respect to x-axis (gamma0)
+
+
+%___ Kinematic Condition
+Toe_fl              = -0*pi/180;
+Toe_fr              = -Toe_fl;
+Toe_rl              = 0*pi/180;
+Toe_rr              = -Toe_rl; 
+
+t_front             = t; %[m] track width front
+t_rear              = t; %[m] track width rear
---
+++ double_track_16_06_2025/runSIM.m
@@ -0,0 +1,17 @@
+initParam;
+
+%% Time of Simulation
+time_step_size       = 0.001; % [s] time step of a simulation
+
+time_simulated       = 40; % [s] time of simulation
+
+%% INITIAL CONDITION
+
+%_____ Velocity and Yaw Rate
+u0                   = 0; 
+v0                   = 0;
+yaw_rate0            = 0; %[rad/s] yaw rate at start
+
+
+%% SIMULATION AND RESULTS
+out                = sim("DTM_sim.slx");
